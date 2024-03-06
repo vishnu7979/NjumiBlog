23fa081b96b49e023e4e3ad1dea2540437d19878
vishnu <vishnushaji797@gmail.com>
2024-03-06 15:21:49 +0530

    Create SignUp Page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -35,7 +35,7 @@ export default function Header() {
         <Button className='w-12 h-10 hidden sm:inline' color='gray' pill> 
             <FaMoon />
         </Button>
-        <Link to='/sign-in'>
+        <Link to='/signin'>
             <Button gradientDuoTone='purpleToBlue' outline>
               Sign In
             </Button>
