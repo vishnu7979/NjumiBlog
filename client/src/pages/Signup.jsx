036d8c479af56ed0bcd62b6dcd2a8c1f0aae257b
vishnu <vishnushaji797@gmail.com>
2024-03-06 22:23:43 +0530

    Complete signin page functionality

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -111,7 +111,7 @@ export default function Signup() {
 
           <div className='flex gap-2 text-sm mt-5 '>
             <span>Have an account?</span>
-            <Link to='/sign-in' className='text-blue-500'>
+            <Link to='/signin' className='text-blue-500'>
               Sign In
             </Link>
           </div>
