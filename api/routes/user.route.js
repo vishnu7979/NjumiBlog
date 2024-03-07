33aec50f6715ac3e47c20041c2b02ba4df32aa1d
vishnu <vishnushaji797@gmail.com>
2024-03-07 21:38:11 +0530

    Add delete user API route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import {
-//   deleteUser,
+  deleteUser,
 //   getUser,
 //   getUsers,
 //   signout,
@@ -15,7 +15,7 @@ router.get('/test', test);
 router.put('/update/:userId',
  verifyToken,
   updateUser);
-// router.delete('/delete/:userId', verifyToken, deleteUser);
+router.delete('/delete/:userId', verifyToken, deleteUser);
 // router.post('/signout', signout);
 // router.get('/getusers', verifyToken, getUsers);
 // router.get('/:userId', getUser);
