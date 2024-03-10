02db2e8dae7d9b037a2e63cf6554b20b40211cf6
vishnu <vishnushaji797@gmail.com>
2024-03-10 21:40:51 +0530

    Add show more functionality to the posts results of the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -31,23 +31,23 @@ export default function DashPosts() {
     }
   }, [currentUser._id]);
 
-//   const handleShowMore = async () => {
-//     const startIndex = userPosts.length;
-//     try {
-//       const res = await fetch(
-//         `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
-//       );
-//       const data = await res.json();
-//       if (res.ok) {
-//         setUserPosts((prev) => [...prev, ...data.posts]);
-//         if (data.posts.length < 9) {
-//           setShowMore(false);
-//         }
-//       }
-//     } catch (error) {
-//       console.log(error.message);
-//     }
-//   };
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(
+        `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts]);
+        if (data.posts.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
 //   const handleDeletePost = async () => {
 //     setShowModal(false);
@@ -133,14 +133,14 @@ export default function DashPosts() {
               </Table.Body>
             ))}
           </Table>
-          {/* {showMore && (
+          {showMore && (
             <button
               onClick={handleShowMore}
               className='w-full text-teal-500 self-center text-sm py-7'
             >
               Show more
             </button>
-          )} */}
+          )}
         </>
       ) : (
         <p>You have no posts yet!</p>
