d4562457d8825e37089e58afdc184442d5cf53f7
vishnu <vishnushaji797@gmail.com>
2024-03-13 11:30:29 +0530

    add pre-installation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,10 @@ import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from "cookie-parser";
+import path, { dirname } from "path";
+
+
+
 dotenv.config();
 
 mongoose
@@ -18,6 +22,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+  const __dirname = path.resolve();
 
 const app = express();
 
@@ -33,6 +39,12 @@ app.use('/api/user',userRoutes)
 app.use('/api/auth',authRoutes)
 app.use('/api/post',postRoutes)
 app.use('/api/comment', commentRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
